9f373d71ff9bad078463e72893a5b34a006c605a
ROBSDII <robert_dohring@yahoo.com>
2016-07-18 18:54:12 +0100

    2 new pages

--- Home.php
+++ Home.php
@@ -9,11 +9,26 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Page 1</a></li>
+            <li><a href="#">Page 2</a></li>
+            <li><a href="#">Page 3</a></li>
+        </ul>
+    </div>
+</nav>
+
 <div class="container">
     <h1>My First Bootstrap Page</h1>
     <p>This part is inside a .container class.</p>
     <p>The .container class provides a responsive fixed width container.</p>
 </div>
+
 
 </body>
 <?php
