c1d94b37cd05a6943bf45e5eaec9d05adacdda32
ROBSDII <robert_dohring@yahoo.com>
2016-07-18 04:01:56 +0100

    2 new pages

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+
+
+<?php
+$db = new mysqli(
+//Azure link
+    "us-cdbr-azure-west-c.cloudapp.net",
+    //Azure Username
+    "b790e3d6643f83",
+    //Azure Password
+    "ad68dd37",
+    //Azure Database name
+    "1313768data"
+);
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
