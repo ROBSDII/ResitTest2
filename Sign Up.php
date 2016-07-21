4d137ee216514b281485b1adc502c338e4fe459c
ROBSDII <robert_dohring@yahoo.com>
2016-07-21 15:25:55 +0100

    added Home href link

--- Sign Up.php
+++ Sign Up.php
@@ -15,7 +15,7 @@
             <a class="navbar-brand" href="#">BigBugFinder</a>
         </div>
         <ul class="nav navbar-nav navbar-center">
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="Home.php">Home</a></li>
             <li><a href="#">Page 1</a></li>
             <li><a href="#">Page 2</a></li>
             <li><a href="#">Page 3</a></li>
