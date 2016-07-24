aecafe4ff1aadc27dadb3dac46a178b8d9f94188
ROBSDII <robert_dohring@yahoo.com>
2016-07-24 02:13:09 +0100

    displaying bugs

--- Home.php
+++ Home.php
@@ -45,7 +45,22 @@
 
 
 <?php
-include('db_connect.php');
+$db = new mysqli(
+//Azure link
+    "us-cdbr-azure-west-c.cloudapp.net",
+    //Azure Username
+    "b790e3d6643f83",
+    //Azure Password
+    "ad68dd37",
+    //Azure Database name
+    "1313768data"
+);
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}else{
+    echo"Success";
+}
 $uniquequery = "SELECT title,username,datePosted FROM bugs ORDER BY datePosted DESC LIMIT 5";
 $sqldb = mysqli_query($db, $uniquequery) or die('Invalid query');
 if (!$sqldb) {
