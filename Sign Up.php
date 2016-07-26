0ac31b3ba27ccb9eba62558bcd139c6a1597fc87
ROBSDII <robert_dohring@yahoo.com>
2016-07-26 15:18:28 +0100

    displaying bugs

--- Sign Up.php
+++ Sign Up.php
@@ -36,7 +36,7 @@
 
 </nav>
     <div class="container padding-top-40">
-        <div class="panel panel-default col-md-6">
+        <div class="panel panel-default col-md-5">
             <div class="panel-heading">
                 Registration
             </div>
