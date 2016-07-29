080f2517ffbae388ddf29e748d45c3fa272fc846
ROBSDII <robert_dohring@yahoo.com>
2016-07-29 16:55:19 +0100

    trying to get register to work

--- Sign Up.php
+++ Sign Up.php
@@ -64,17 +64,15 @@
         </div>
     </div>
 </body>
-
-</html>
 <?php
 //Azure link
-    $dbserver="us-cdbr-azure-west-c.cloudapp.net";
-    //Azure Username
-    $dbusername="b790e3d6643f83";
-    //Azure Password
-    $dbpassword="ad68dd37";
-    //Azure Database name
-    $dbname="1313768data";
+$dbserver="us-cdbr-azure-west-c.cloudapp.net";
+//Azure Username
+$dbusername="b790e3d6643f83";
+//Azure Password
+$dbpassword="ad68dd37";
+//Azure Database name
+$dbname="1313768data";
 $db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
 // test if connection was established, and print any errors
 if (!$db) {
@@ -89,9 +87,10 @@ if(isset($_POST['submit'])){
     $username=mysqli_real_escape_string($db,$_POST['username']);
     $password=mysqli_real_escape_string($db,$_POST['password']);
 
-if($username=="" OR $name=="" OR $country=="" OR $password==""){
-    echo "<script>alert('Please fill username')</script>";
-    //exit();
+    if($username=="" OR $name=="" OR $country=="" OR $password==""){
+        echo "<script>alert('Please fill username')</script>";
+        //exit();
     }
 }
 ?>
+</html>
