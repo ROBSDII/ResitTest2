675c713599282449ba4c47de272e58fb15e3e007
ROBSDII <robert_dohring@yahoo.com>
2016-07-29 16:54:25 +0100

    trying to get register to work

--- Sign Up.php
+++ Sign Up.php
@@ -67,7 +67,21 @@
 
 </html>
 <?php
-include('db_connect.php');
+//Azure link
+    $dbserver="us-cdbr-azure-west-c.cloudapp.net";
+    //Azure Username
+    $dbusername="b790e3d6643f83";
+    //Azure Password
+    $dbpassword="ad68dd37";
+    //Azure Database name
+    $dbname="1313768data";
+$db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}else{
+    echo"PLease work";
+}
 echo "Please fill username";
 if(isset($_POST['submit'])){
     $name=mysqli_real_escape_string($db,$_POST['name']);
