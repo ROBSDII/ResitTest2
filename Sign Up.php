b63f6110b8e4baab0db2226eb43ca346f163ad08
ROBSDII <robert_dohring@yahoo.com>
2016-07-29 17:01:40 +0100

    trying to get register to work

--- Sign Up.php
+++ Sign Up.php
@@ -59,38 +59,39 @@
                                 <input type="submit" value="Submit"/>
                         </div>
                     </div>
+                    <?php
+                    //Azure link
+                    $dbserver="us-cdbr-azure-west-c.cloudapp.net";
+                    //Azure Username
+                    $dbusername="b790e3d6643f83";
+                    //Azure Password
+                    $dbpassword="ad68dd37";
+                    //Azure Database name
+                    $dbname="1313768data";
+                    $db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
+                    // test if connection was established, and print any errors
+                    if (!$db) {
+                        die('Connect Error: ' . mysqli_connect_errno());
+                    }else{
+                        echo"PLease work";
+                    }
+                    echo "Please fill all";
+                    if(isset($_POST['submit'])){
+                        $name=mysqli_real_escape_string($db,$_POST['name']);
+                        $country=mysqli_real_escape_string($db,$_POST['country']);
+                        $username=mysqli_real_escape_string($db,$_POST['username']);
+                        $password=mysqli_real_escape_string($db,$_POST['password']);
+
+                        if($username=="" OR $name=="" OR $country=="" OR $password==""){
+                            echo "Please fill username";
+                            //exit();
+                        }
+                    }
+                    ?>
                 </form>
             </div>
         </div>
     </div>
 </body>
-<?php
-//Azure link
-$dbserver="us-cdbr-azure-west-c.cloudapp.net";
-//Azure Username
-$dbusername="b790e3d6643f83";
-//Azure Password
-$dbpassword="ad68dd37";
-//Azure Database name
-$dbname="1313768data";
-$db = new mysqli($dbserver, $dbusername, $dbpassword, $dbname);
-// test if connection was established, and print any errors
-if (!$db) {
-    die('Connect Error: ' . mysqli_connect_errno());
-}else{
-    echo"PLease work";
-}
-echo "Please fill all";
-if(isset($_POST['submit'])){
-    $name=mysqli_real_escape_string($db,$_POST['name']);
-    $country=mysqli_real_escape_string($db,$_POST['country']);
-    $username=mysqli_real_escape_string($db,$_POST['username']);
-    $password=mysqli_real_escape_string($db,$_POST['password']);
 
-    if($username=="" OR $name=="" OR $country=="" OR $password==""){
-        echo "Please fill username";
-        //exit();
-    }
-}
-?>
 </html>
