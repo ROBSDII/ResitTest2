ad06a70bdb2447d81e064ae8a0fa1054c933bfc4
ROBSDII <robert_dohring@yahoo.com>
2016-07-29 14:24:50 +0100

    displaying bugs

--- db_connect.php
+++ db_connect.php
@@ -1,16 +1,16 @@
 
 
 <?php
-$db = new mysqli(
+
 //Azure link
-    "us-cdbr-azure-west-c.cloudapp.net",
+   $db_host="us-cdbr-azure-west-c.cloudapp.net";
     //Azure Username
-    "b790e3d6643f83",
+    $db_username="b790e3d6643f83";
     //Azure Password
-    "ad68dd37",
+    $db_password="ad68dd37";
     //Azure Database name
-    "1313768data"
-);
+    $db_name="1313768data";
+$db =mysqli_connect($db_host,$db_username,$db_password,$db_name);
 // test if connection was established, and print any errors
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
