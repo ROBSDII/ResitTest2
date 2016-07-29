789ccffd1e18b575c0fad0711502a4766648749b
ROBSDII <robert_dohring@yahoo.com>
2016-07-29 13:59:17 +0100

    displaying bugs

--- signUpData.php
+++ signUpData.php
@@ -1,5 +1,20 @@
 <?php
-include("db_connect.php");
+$db = new mysqli(
+//Azure link
+    "us-cdbr-azure-west-c.cloudapp.net",
+    //Azure Username
+    "b790e3d6643f83",
+    //Azure Password
+    "ad68dd37",
+    //Azure Database name
+    "1313768data"
+);
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}else{
+    echo"Success";
+}
 
 $name=$_REQUEST['name'];
 $country=$_REQUEST['country'];
