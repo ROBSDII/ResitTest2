15d00f2f5213cd683b816404ceb7ae4b4ff3c5bc
ROBSDII <robert_dohring@yahoo.com>
2016-07-31 23:05:32 +0100

    setting up a navbar page

--- registration.php
+++ registration.php
@@ -10,32 +10,7 @@
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="Home.php">BigBugFinder</a>
-        </div>
-        <ul class="nav navbar-nav navbar-center">
-            <li class="active"><a href="Home.php">Home</a></li>
-            <li><a href="#">Search</a></li>
-            <li><a href="#">Page 2</a></li>
-            <li><a href="#">Page 3</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-            <form class="navbar-form" role="search">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search" name="q">
-                    <div class="input-group-btn">
-                        <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-                    </div>
-                </div>
-            </form>
-        </ul>
-    </div>
-
-</nav>
+<?php include 'navbar.php';?>
 
 <br><br><br>
 
