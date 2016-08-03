c0decf3258b3ff7d69e65fce13cc00c4668ec394
ROBSDII <robert_dohring@yahoo.com>
2016-08-03 14:32:24 +0100

    setting up bug title links

--- signUpData.php
+++ signUpData.php
@@ -19,7 +19,7 @@ session_start();
 include('db_connect.php');
 
 echo $bug_title=$_GET['bug_title'];
-$sqlgrab = "SELECT bugs.bugTitle,bugs.bugDescription , bugs.datePosted FROM bugs WHERE bugs.bugTitle='.$bug_title.'";
+$sqlgrab = "SELECT bugs.bugTitle,bugs.bugDescription , bugs.datePosted FROM bugs WHERE bugs.bugTitle=$bug_title";
 $sqldb = mysqli_query($db, $sqlgrab) or die('Invalid query');
 
 echo "<table>";
