0aab2619a1f3cb25022b093715f6254259a79893
ROBSDII <robert_dohring@yahoo.com>
2016-08-05 22:18:52 +0100

    setting up the search bar

--- Home.php
+++ Home.php
@@ -7,9 +7,10 @@
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <?php/*including navbar for avg users*/  include 'navbar.php';?>
 </head>
 <body>
-<?php/*including navbar for avg users*/ session_write_close(); include 'navbar.php';?>
+
 
 <div class="container">
     <h1>Welcome to BigBugFinder</h1>
